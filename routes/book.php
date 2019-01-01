d950464507535c9bf9e7b04422e2e95f4be5d9ec
Rodrigo <rodrigo.werlich@gmail.com>
2019-01-01 09:13:06 -0200

    rotas

--- routes/book.php
+++ routes/book.php
@@ -3,24 +3,20 @@
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/books', function (Request $request, Response $response) {
+$app->get('/books[/{id}]', function (Request $request, Response $response) {
     return $this->BookController->findAll($request, $response);
 });
 
-$app->get('/book/{id}', function (Request $request, Response $response) use ($app) {
-    return $this->BookController->find($request, $response);
-});
-
-$app->post('/book', function (Request $request, Response $response) use ($app) {
+$app->post('/books', function (Request $request, Response $response) use ($app) {
     return $this->BookController->save($request, $response);
 });
 
-$app->put('/book/{id}', function (Request $request, Response $response) use ($app) {
+$app->put('/books/{id}', function (Request $request, Response $response) use ($app) {
     return $this->BookController->save($request, $response);
 });
 
 
-$app->delete('/book/{id}', function (Request $request, Response $response) use ($app) {
+$app->delete('/books/{id}', function (Request $request, Response $response) use ($app) {
     return $this->BookController->delete($request, $response);
 });
 
